8452330a85f49850a0cf70259993c94a07255e48
M1nhBaka <minhhoc2k4@gmail.com>
2025-04-11 16:47:55 +0700

    update watchingMovie

--- src/main/java/com/movieflix/controllers/VideoStreamingController.java
+++ src/main/java/com/movieflix/controllers/VideoStreamingController.java
@@ -2,20 +2,20 @@ package com.movieflix.controllers;
 
 import com.movieflix.service.VideoStreamingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 @RestController
-@RequestMapping("/api/video")
+@RequestMapping("/api/v1/video")
 public class VideoStreamingController {
     @Autowired
     private VideoStreamingService videoStreamingService;
 
     @GetMapping("/stream/{movieId}")
-    public ResponseEntity<StreamingResponseBody> streamVideo(
-            @PathVariable Integer movieId,
-            @RequestHeader(value = "Range", required = false) String range) {
-        return videoStreamingService.streamVideo(movieId, range);
+    public ResponseEntity<Resource> streamVideo(
+            @PathVariable Integer movieId) {
+        return videoStreamingService.streamVideo(movieId);
     }
 } 
